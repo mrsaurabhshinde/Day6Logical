a1ef7fcd05fd62124517ac934db584167fdc7f90
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-06 10:53:57 +0530

    compute the square root of a nonnegative number C

---
+++ SquareRoot.java
@@ -0,0 +1,24 @@
+package com.bl.Junit;
+
+import java.util.Scanner;
+
+public class SquareRoot
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter non negative number : ");
+
+        int c = sc.nextInt();
+
+        int t = c;
+
+        while (Math.abs(t-(c/t))> Math.pow(1,-15)){  //where episilon=1e-15
+
+            t = ((c/t)+ t)/2;
+
+        }
+
+        System.out.println("the square root of given number is : " + t);
+    }
+}
