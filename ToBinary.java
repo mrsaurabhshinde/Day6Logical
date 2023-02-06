1add5ef3a88934fcbfe7c5affeaeca499d2b3ef7
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-06 10:57:01 +0530

    To compute the binary representation of n

---
+++ ToBinary.java
@@ -0,0 +1,22 @@
+package com.bl.Junit;
+
+import java.util.Scanner;
+
+public class ToBinary
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int a = sc.nextInt();
+        tobinary(a);
+    }
+    static void tobinary(int n) {
+        if(n == 0) {
+            return;
+        }
+        int remainder = n%2;
+        n = n/2;
+        tobinary(n);
+        System.out.printf("%d",remainder);
+    }
+}
